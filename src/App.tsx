a2aac565f408abfdac13fb54228ea6bfd4df5399
Nathan-PO <nathanlacerda224@gmail.com>
2026-03-26 17:56:38 -0300

    Renamed "HomePage" for "CreateUserPage" and created screen of Create User.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { Link, Navigate, Route, Routes } from "react-router-dom";
 import { AboutPage } from "./pages/AboutPage";
-import { HomePage } from "./pages/HomePage";
+import { CreateUserPage } from "./pages/CreateUserPage";
 import "./styles/app.scss";
 
 function App() {
   return (
     <div className="app">
       <header className="app__header">
-        <h1>MVP React + Vite</h1>
+        <h1 className="page-title">CHSRC Construct</h1>
         <nav className="app__nav">
-          <Link to="/">Home</Link>
-          <Link to="/sobre">Sobre</Link>
+          {/* <Link to="/">Home</Link> */}
+          {/* <Link to="/sobre">Sobre</Link> */}
         </nav>
       </header>
 
       <main className="app__content">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<CreateUserPage />} />
           <Route path="/sobre" element={<AboutPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
--- src/pages/HomePage.tsx
+++
@@ -1,8 +0,0 @@
-export function HomePage() {
-  return (
-    <section>
-      <h2>Home</h2>
-      <p>Tela inicial mínima para o MVP.</p>
-    </section>
-  );
-}
