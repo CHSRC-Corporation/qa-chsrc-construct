36f09e2feb61b8ca9a1858607c2b9b44a2f9c719
Victor Hugo <victor.coelho@a.catolica-to.edu.br>
2026-04-23 02:30:17 -0300

    Corrige o script de teste

--- src/soma.test.js
+++ src/soma.test.js
@@ -1,9 +1,6 @@
-const test = require('node:test');
-
-const assert = require('node:assert');
-
+import { test } from 'node:test';
+import assert from 'node:assert';
 
 test('Validação de segurança do sistema', () => {
   assert.strictEqual(1 + 1, 2);
-
 });
